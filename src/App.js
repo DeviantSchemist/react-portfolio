d03d94c523bb05ebccf8f6d7e2ab57c9c2183f1b
Michael Ngo <michaelngo1337@yahoo.com>
2021-07-13 22:21:15 -0700

    made route for About page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import NavbarComponent from './components/Navbar'
 import About from './pages/About'
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
 
 const App = () => {
   return (
     <>
-      <NavbarComponent />
-      <About />
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/">
+            <NavbarComponent />
+            <About />
+          </Route>
+        </Switch>
+      </BrowserRouter>
     </>
   )
 }
