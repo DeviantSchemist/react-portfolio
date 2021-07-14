b5eacc7f98b0f1c9accdd1d292d5b78cc57a4a11
Michael Ngo <michaelngo1337@yahoo.com>
2021-07-14 13:57:19 -0700

    made contact page outline

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import Header from './components/Header'
 import Footer from './components/Footer'
 import About from './pages/About'
 import Projects from './pages/Projects'
+import Contact from './pages/Contact'
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
 
 const App = () => {
@@ -22,6 +23,11 @@ const App = () => {
         <Switch>
           <Route path="/projects">
             <Projects />
+          </Route>
+        </Switch>
+        <Switch>
+          <Route path="/contact">
+            <Contact />
           </Route>
         </Switch>
       </BrowserRouter>
--- src/pages/Contact/Contact.js
+++ src/pages/Contact/Contact.js
@@ -1,7 +1,34 @@
+import { FormControl, InputLabel, Input, FormHelperText, Typography, makeStyles, TextareaAutosize } from '@material-ui/core'
 
+const useStyles = makeStyles({
+  forms: {
+    display: 'block'
+  }
+})
 
 const Contact = () => {
+  const classes = useStyles()
 
+  return (
+    <>
+      <Typography variant="h2">
+        Contact
+      </Typography>
+
+      <FormControl className={classes.forms}>
+        <InputLabel htmlFor="my-input">Name</InputLabel>
+        <Input id="my-input" aria-describedby="my-helper-text" />
+      </FormControl>
+
+      <FormControl className={classes.forms}>
+        <InputLabel htmlFor="my-input">Email address</InputLabel>
+        <Input id="my-input" aria-describedby="my-helper-text" />
+        <FormHelperText id="my-helper-text">We'll never share your email.</FormHelperText>
+      </FormControl>
+
+      <TextareaAutosize aria-label="minimum height" minRows={3} placeholder="Minimum 3 rows" />
+    </>
+  )
 }
 
 export default Contact
