c1f7a276cc09edb3e47b2b89d76be4c9f5952178
Michael Ngo <michaelngo1337@yahoo.com>
2021-07-14 21:43:54 -0700

    aligned project elements

--- src/pages/Projects/Projects.js
+++ src/pages/Projects/Projects.js
@@ -7,6 +7,8 @@ import CardMedia from '@material-ui/core/CardMedia'
 import Button from '@material-ui/core/Button'
 import Typography from '@material-ui/core/Typography'
 import Grid from '@material-ui/core/Grid';
+import firstProjectImage from '../../Assets/Project1.png'
+import secondProjectImage from '../../Assets/ComicCol.png'
 
 const useStyles = makeStyles({
   root: {
@@ -17,6 +19,10 @@ const useStyles = makeStyles({
   },
   grid: {
     marginTop: '10%'
+  },
+  typo: {
+    textAlign: 'center',
+    marginTop: '-4%'
   }
 })
 
@@ -26,76 +32,86 @@ const Projects = () => {
   return (
     <>
       <Grid container spacing={10} justifyContent="center" className={classes.grid}>
-        <Grid item>
+        <Grid item xs={12}>
+          <Typography variant="h2" className={classes.typo}> Projects </Typography>
+        </Grid>
+
+        <Grid item xs={3}>
           <Card className={classes.root}>
             <CardActionArea>
               <CardMedia
                 className={classes.media}
-                image="/static/images/cards/contemplative-reptile.jpg"
-                title="Contemplative Reptile"
+                image={firstProjectImage}
+                title="First Project"
               />
               <CardContent>
                 <Typography gutterBottom variant="h5" component="h2">
                   Project 1
                 </Typography>
                 <Typography variant="body2" color="textSecondary" component="p">
-                  This is my first project.
+                  This is a food finder application. Unique in that users will search for a particular
+                  food they want, and the app displays to them recipes for that food as well as nearby
+                  restaurants that serve that food. Users can then favorite either recipes or
+                  restaurant locations for future lookup.
                 </Typography>
               </CardContent>
             </CardActionArea>
             <CardActions>
-              <Button size="small" color="primary">
+              <Button href="https://github.com/DeviantSchemist/Project1" size="small" color="primary">
                 Repo
               </Button>
-              <Button size="small" color="primary">
+              <Button href="https://deviantschemist.github.io/Project1/" size="small" color="primary">
                 Deployed
               </Button>
             </CardActions>
           </Card>
         </Grid>
 
-        <Grid item>
+        <Grid item xs={3}>
           <Card className={classes.root}>
             <CardActionArea>
               <CardMedia
                 className={classes.media}
-                image="/static/images/cards/contemplative-reptile.jpg"
-                title="Contemplative Reptile"
+                image={secondProjectImage}
+                title="Second Project"
               />
               <CardContent>
                 <Typography gutterBottom variant="h5" component="h2">
                   Project 2
                 </Typography>
                 <Typography variant="body2" color="textSecondary" component="p">
-                  This is my second project.
+                  A comic collection application. Users can search for comics and add ones that
+                  they like to their favorites list. They can also see comments on particular
+                  comics that they search for. They can send the developers feedback using the
+                  side navigation bar.
                 </Typography>
               </CardContent>
             </CardActionArea>
             <CardActions>
-              <Button size="small" color="primary">
+              <Button href="https://github.com/CharlotteFrancis/ComicCol" size="small" color="primary">
                 Repo
               </Button>
-              <Button size="small" color="primary">
+              <Button href="https://comiccol.herokuapp.com/" size="small" color="primary">
                 Deployed
               </Button>
             </CardActions>
           </Card>
         </Grid>
 
-        <Grid item>
+        <Grid item xs={3}>
           <Card className={classes.root}>
             <CardActionArea>
               <CardMedia
                 className={classes.media}
                 image="/static/images/cards/contemplative-reptile.jpg"
-                title="Contemplative Reptile"
+                title="Third Project"
               />
               <CardContent>
                 <Typography gutterBottom variant="h5" component="h2">
                   Project 3
                 </Typography>
                 <Typography variant="body2" color="textSecondary" component="p">
-                  This is my third project.
+                  This is my third project. Have yet to finish it.
                 </Typography>
               </CardContent>
             </CardActionArea>
