85f876146a8a6e09c147f42cde62980e50a4ffdb
Michael Ngo <michaelngo1337@yahoo.com>
2021-07-14 22:01:36 -0700

    made footer nice

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,4 +1,4 @@
-import { AppBar, Container, Toolbar, Typography, makeStyles } from '@material-ui/core'
+import { AppBar, Container, Toolbar, Typography, makeStyles, Grid } from '@material-ui/core'
 import GitHubIcon from '@material-ui/icons/GitHub'
 import FacebookIcon from '@material-ui/icons/Facebook'
 import LinkedInIcon from '@material-ui/icons/LinkedIn'
@@ -15,7 +15,8 @@ const useStyles = makeStyles({
     color: 'white',
     '&:hover': {
     color: 'yellow'
-    }
+    },
+    marginLeft: '2%'
   }
 })
 
@@ -27,8 +28,9 @@ const Footer = () => {
       <Container maxWidth="md">
         <Toolbar>
           <Typography variant="body1" color="inherit">
-            © 2019 Michael Ngo
+            © 2021 Michael Ngo
           </Typography>
+          <Grid container direction="row" item xs={8}></Grid>
           <GitHubIcon onClick={() => window.open('https://github.com/DeviantSchemist')} className={classes.clickableIcon} />
           <FacebookIcon onClick={() => window.open('https://www.facebook.com/michael.ngo.9')} className={classes.clickableIcon} />
           <LinkedInIcon onClick={() => window.open('https://www.facebook.com/michael.ngo.9')} className={classes.clickableIcon}/>
